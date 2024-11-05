22dead8584e11b9ab1ebb449d0900a54d0b2df44
Chris Fesler <cfesler@nutshelllabs.io>
2024-11-05 12:18:51 -0800

    tmux

---
+++ dot_wezterm.lua
@@ -0,0 +1,20 @@
+local wezterm = require("wezterm")
+local config = wezterm.config_builder()
+
+function scheme_for_appearance(appearance)
+	if appearance:find("Dark") then
+		return "Catppuccin Mocha"
+	else
+		return "Catppuccin Macchiato"
+	end
+end
+
+-- This is where you actually apply your config choices
+config.color_scheme = scheme_for_appearance(wezterm.gui.get_appearance())
+config.font = wezterm.font("FiraMono Nerd Font")
+config.font_size = 16
+config.enable_tab_bar = true
+config.window_decorations = "RESIZE"
+
+-- and finally, return the configuration to wezterm
+return config
