43cd5f08a7f44f140bf41718b202efee7f5a28ab
Chris Fesler <cfesler@nutshelllabs.io>
2025-04-08 17:12:52 -0700

    update .zshrc to be more portable

--- dot_oh-my-zsh/custom/fd.zsh
+++ dot_oh-my-zsh/custom/fd.zsh
@@ -1,5 +1,5 @@
 # "sharkdp/fd" file finder, modern replacement for GNU find
-export FD_OPTIONS="--hidden --follow --ignore-file $DOTFILES/fd/.ignore"
+export FD_OPTIONS="--hidden --follow"
 
 # alias fd='fd --hidden --follow --ignore-file "$DOTFILES/.ignore"'
 alias fd="fd $FD_OPTIONS"
